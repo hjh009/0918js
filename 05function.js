82cdb282ba325d7dd98e9cd7b49a461a73c029b7
한재호 <wogh009@gmail.com>
2025-09-19 14:13:48 +0900

    .

---
+++ 05function.js
@@ -0,0 +1,13 @@
+
+const func = function () {
+  console.log("func");
+};
+
+
+const arrow = () => {
+  console.log("arrow");
+};
+
+const paramiter1 = a => {
+  console.log(a);
+};
---
+++ structure2.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4];
+
+// 일반 for문
+let result = 0;
+for (i = 0; i < numbers.length; i++) {
+  result += numbers[i];
+}
+console.log(result); // 20;
